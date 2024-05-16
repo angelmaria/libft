34e7452b2de4947d7ef0c94979f628fdf12c6898
Angel Martinez <angelmaria75@gmail.com>
2024-05-17 01:52:54 +0200

    cambios adiciones en varias funciones extra

---
+++ ft_strchr.c
@@ -0,0 +1,35 @@
+#include "libft.h"
+
+char	*ft_strchr(const char *s, int i)
+{
+	while (*s)
+	{
+		if (*s == i)
+			return ((char *)s);
+		s++;
+	}
+	if (i == '\0')
+		return ((char *)s);
+	return (0);
+}
+// o este otro código
+char		*ft_strchr(const char *s, int c)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i])
+	{
+		if (s[i] == c)
+			return ((char *)s + i);
+		i++;
+	}
+	if (!c && s[i] == '\0')
+		return ((char *)s + i);
+	return (NULL);
+}
+// o este otro código
+char	*ft_strchr(const char *s, int c)
+{
+	return (ft_memchr(s, c, ft_strlen(s) + 1));
+}
--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -12,4 +12,44 @@
 
 #include "libft.h"
 
+size_t	ft_strlcpy(char *dest, const char *src, size_t size)
+{
+	size_t	i;
 
+	i = 0;
+	if (size == 0)
+	{
+		while (src[i])
+			i++;
+		return (i);
+	}
+	while (i < size - 1 && src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	if (i < size)
+		dest[i] = '\0';
+	while (src[i] != '\0')
+		i++;
+	return (i);
+}
+// Test (borrar)
+int main(void)
+{
+    char src[] = "Copilot es asombroso!";
+    char dest[19];
+    size_t copied;
+
+    // Prueba la función ft_strlcpy con un tamaño de buffer suficiente
+    copied = ft_strlcpy(dest, src, sizeof(dest));
+    printf("Cadena copiada: '%s'\n", dest);
+    printf("Longitud de la cadena original: %zu\n", copied);
+
+    // Prueba la función ft_strlcpy con un tamaño de buffer más pequeño que la cadena de origen
+    copied = ft_strlcpy(dest, src, 10);
+    printf("Cadena copiada con buffer limitado: '%s'\n", dest);
+    printf("Longitud de la cadena original: %zu\n", copied);
+
+    return 0;
+}
