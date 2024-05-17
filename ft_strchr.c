a6d1e1d81af1cdd9c19b4c74dff5dc6499306c1a
Angel Martinez <angelmaria75@gmail.com>
2024-05-17 02:05:10 +0200

    añadindo alguna función más a la librería

--- ft_strchr.c
+++ ft_strchr.c
@@ -12,6 +12,29 @@ char	*ft_strchr(const char *s, int i)
 		return ((char *)s);
 	return (0);
 }
+// Test (borrar)
+int	main(void)
+{
+	const char *cadena = "Hola Mundo!";
+	char caracter_a_buscar = 'M';
+	char *resultado;
+
+	resultado = ft_strchr(cadena, caracter_a_buscar);
+	if (resultado != NULL)
+		printf("Caracter encontrado: %s\n", resultado);
+	else
+		printf("Caracter no encontrado.\n");
+
+	// Prueba con el carácter nulo
+	resultado = ft_strchr(cadena, '\0');
+	if (resultado != NULL)
+		printf("Caracter nulo encontrado al final de la cadena: %s\n", resultado);
+	else
+		printf("Caracter nulo no encontrado.\n");
+
+	return (0);
+}
+
 // o este otro código
 char		*ft_strchr(const char *s, int c)
 {
